db03c17cb977c6ee49ade74fb79a1a731e0c96f2
Sayuri Travezano <sayuri.travezano@tecsup.edu.pe>
2025-06-22 04:36:54 +0100

    Primera entrega del proyecto BiblioGestor

---
+++ resources/views/test.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Vista de prueba - BiblioGestor</title>
+</head>
+<body>
+    <h1>Bienvenida Sayuri 📚</h1>
+    <p>Tu sistema de gestión de libros ya está funcionando 🚀</p>
+</body>
+</html>
